e9a2c8a96434d25cfd5588f48b2d47029dd090a2
starving-marvin <grishin-ilya@outlook.com>
2020-11-05 23:07:54 -0800

    Write a C program to print the following characters in a reverse way

--- main.c
+++ main.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (void) {
 
-    //  Write a C program to get the C version you are using
-    switch (__STDC_VERSION__) {
-        case 199901L:
-            printf("We are using C99!\n");
-            break ;
-
-        case 201112L:
-            printf("We are using C11!\n");
-            break ;
+    // Write a C program to print the following characters in a reverse way. Go to the editor
+    // Test Characters: 'X', 'M', 'L' 
     
-        default:
-            printf("We are using C90!\n");
+    char* string = "XML";
+
+    for (int i = strlen(string) - 1; i >= 0; i--) {
+        putchar(string[i]);
     }
+
+    printf("\n");
 
     return EXIT_SUCCESS;
 }
