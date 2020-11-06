60e0f8188a11a5975fdc02cb1076cc4c6bbb8576
starving-marvin <grishin-ilya@outlook.com>
2020-11-05 23:19:14 -0800

    Write a C program to compute the perimeter and area of a rectangle

--- main.c
+++ main.c
@@ -1,19 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <assert.h>
+
+float area(float a, float b);
+
+float per(float a, float b);
 
 int main (void) {
 
-    // Write a C program to print the following characters in a reverse way. Go to the editor
-    // Test Characters: 'X', 'M', 'L' 
-    
-    char* string = "XML";
+    // Write a C program to compute the perimeter and area of a rectangle with a height of 7 inches, and width of 5 inches
+    float inch = 7;
+    float width = 5;
 
-    for (int i = strlen(string) - 1; i >= 0; i--) {
-        putchar(string[i]);
-    }
-
-    printf("\n");
+    printf("Area:\t%.1f\nPerimeter:\t%.1f\n", area(inch, width), per(inch, width));
 
     return EXIT_SUCCESS;
+}
+
+float area(float a, float b)
+{   
+    float area = a * b;
+    assert(area >= 0);
+    return area;
+}
+
+float per(float a, float b)
+{
+    float per = 2 * (a + b);
+    assert(per >= 0);
+    return per;
 }
